04e62820dfe222a9d9e388f87a2f60b1a11050f9
Lee <lee.doolan@gmail.com>
2015-09-06 09:29:54 -0700

    remove debug code

--- app.py
+++ app.py
@@ -16,32 +16,27 @@ feat_props= ("id", "price", "street", "status", "bedrooms", "bathrooms", "sq_ft"
 
 @app.route('/listings')
 def listings():
-    ##ary= Listing.query.all()
-    try:
-        l_query= Listing.query
-        min_price= request.args.get('min_price', type=int)
-        if min_price is not None:
-            l_query= l_query.filter(Listing.price >= min_price)
-        max_price= request.args.get('max_price', type=int)
-        if max_price is not None:
-            l_query= l_query.filter(Listing.price <= max_price)
+    l_query= Listing.query
+    min_price= request.args.get('min_price', type=int)
+    if min_price is not None:
+        l_query= l_query.filter(Listing.price >= min_price)
+    max_price= request.args.get('max_price', type=int)
+    if max_price is not None:
+        l_query= l_query.filter(Listing.price <= max_price)
 
-        min_bed= request.args.get('min_bed', type=int)
-        if min_bed is not None:
-            l_query= l_query.filter(Listing.bedrooms >= min_bed)
-        max_bed= request.args.get('max_bed', type=int)
-        if max_bed is not None:
-            l_query= l_query.filter(Listing.bedrooms <= max_bed)
+    min_bed= request.args.get('min_bed', type=int)
+    if min_bed is not None:
+        l_query= l_query.filter(Listing.bedrooms >= min_bed)
+    max_bed= request.args.get('max_bed', type=int)
+    if max_bed is not None:
+        l_query= l_query.filter(Listing.bedrooms <= max_bed)
 
-        min_bath= request.args.get('min_bath', type=int)
-        if min_bath is not None:
-            l_query= l_query.filter(Listing.bathrooms >= min_bath)
-        max_bath= request.args.get('max_bath', type=int)
-        if max_bath is not None:
-            l_query= l_query.filter(Listing.bathrooms <= max_bath)
-
-    except Exception, e:
-        print "exception %s" % (str(e))
+    min_bath= request.args.get('min_bath', type=int)
+    if min_bath is not None:
+        l_query= l_query.filter(Listing.bathrooms >= min_bath)
+    max_bath= request.args.get('max_bath', type=int)
+    if max_bath is not None:
+        l_query= l_query.filter(Listing.bathrooms <= max_bath)
 
     features= list()
     for entry in l_query.all():
