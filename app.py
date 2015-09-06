825aa788ac87dffd0004812dc68ae3be9e1b65ad
Lee <lee.doolan@gmail.com>
2015-09-06 09:23:14 -0700

    add more filters

--- app.py
+++ app.py
@@ -26,37 +26,30 @@ def listings():
         if max_price is not None:
             l_query= l_query.filter(Listing.price <= max_price)
 
-##        min_bed= request.args.get('min_bed', type=int)
-##        if min_bed is not None:
-##            l_query= l_query.filter(Listing.min_bed >= min_bed)
-##        max_bed= request.args.get('max_bed', type=int)
-##        if max_bed is not None:
-##            l_query= l_query.filter(Listing.max_bed <= max_bed)
-##
-##        min_bath= request.args.get('min_bath', type=int)
-##        if min_bath is not None:
-##            l_query= l_query.filter(Listing.min_bath >= min_bath)
-##        max_bath= request.args.get('max_bath', type=int)
-##        if max_bath is not None:
-##            l_query= l_query.filter(Listing.max_bath <= max_bath)
+        min_bed= request.args.get('min_bed', type=int)
+        if min_bed is not None:
+            l_query= l_query.filter(Listing.bedrooms >= min_bed)
+        max_bed= request.args.get('max_bed', type=int)
+        if max_bed is not None:
+            l_query= l_query.filter(Listing.bedrooms <= max_bed)
+
+        min_bath= request.args.get('min_bath', type=int)
+        if min_bath is not None:
+            l_query= l_query.filter(Listing.bathrooms >= min_bath)
+        max_bath= request.args.get('max_bath', type=int)
+        if max_bath is not None:
+            l_query= l_query.filter(Listing.bathrooms <= max_bath)
 
     except Exception, e:
         print "exception %s" % (str(e))
-    try:
-        features= list()
-        ary= l_query.all()
-        ##print "%d entried: %s" % (len(q_list), dumper.dumps(q_list))
 
-        for entry in ary:
-            feature = Feature(geometry=Point((entry.long, entry.lat)))
-            feature.properties= {k:getattr(entry, k) for k in feat_props}
-            features.append(feature)
-
-        retcode= jsonify(FeatureCollection(features))
-
-    except Exception, e:
-        print "exception2 %s" % (str(e))
-
+    features= list()
+    for entry in l_query.all():
+        feature = Feature(geometry=Point((entry.long, entry.lat)))
+        feature.properties= {k:getattr(entry, k) for k in feat_props}
+        features.append(feature)
+        
+    retcode= jsonify(FeatureCollection(features))
     return(retcode)
 
 if __name__ == '__main__':
