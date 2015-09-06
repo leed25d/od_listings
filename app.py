944f5dc0b562233f9226ca3c43a19bd17b50ffbb
Lee <lee.doolan@gmail.com>
2015-09-06 08:41:03 -0700

    debug filter

--- app.py
+++ app.py
@@ -16,29 +16,29 @@ feat_props= ("id", "price", "street", "status", "bedrooms", "bathrooms", "sq_ft"
 
 @app.route('/listings')
 def listings():
-    ary= Listing.query.all()
+    ##ary= Listing.query.all()
     try:
         l_query= Listing.query
         min_price= request.args.get('min_price', type=int)
         if min_price is not None:
-            l_query.filter(Listing.min_price >= min_price)
+            l_query= l_query.filter(Listing.min_price >= min_price)
         max_price= request.args.get('max_price', type=int)
         if max_price is not None:
-            l_query.filter(Listing.max_price <= max_price)
+            l_query= l_query.filter(Listing.max_price <= max_price)
 
-        min_bed= request.args.get('min_bed', type=int)
-        if min_bed is not None:
-            l_query.filter(Listing.min_bed >= min_bed)
-        max_bed= request.args.get('max_bed', type=int)
-        if max_bed is not None:
-            l_query.filter(Listing.max_bed <= max_bed)
-
-        min_bath= request.args.get('min_bath', type=int)
-        if min_bath is not None:
-            l_query.filter(Listing.min_bath >= min_bath)
-        max_bath= request.args.get('max_bath', type=int)
-        if max_bath is not None:
-            l_query.filter(Listing.max_bath <= max_bath)
+##        min_bed= request.args.get('min_bed', type=int)
+##        if min_bed is not None:
+##            l_query= l_query.filter(Listing.min_bed >= min_bed)
+##        max_bed= request.args.get('max_bed', type=int)
+##        if max_bed is not None:
+##            l_query= l_query.filter(Listing.max_bed <= max_bed)
+##
+##        min_bath= request.args.get('min_bath', type=int)
+##        if min_bath is not None:
+##            l_query= l_query.filter(Listing.min_bath >= min_bath)
+##        max_bath= request.args.get('max_bath', type=int)
+##        if max_bath is not None:
+##            l_query= l_query.filter(Listing.max_bath <= max_bath)
 
     except Exception, e:
         print "exception %s" % (str(e))
